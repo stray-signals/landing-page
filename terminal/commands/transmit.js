de4f34935391c8bcb65b462b66b72b4d7d24517b
stray-signals <contact@straysignals.dev>
2026-06-29 00:21:33 -0400

    slight wording change

--- terminal/commands/transmit.js
+++ terminal/commands/transmit.js
@@ -1,5 +1,5 @@
 export default {
   name:        'transmit',
   description: 'leave a message for stray',
-  handler:     () => 'transmit: leave a message for stray. type "run transmit" to open the channel.',
+  handler:     () => 'transmit: leave a signal. type "run transmit" to open the channel.',
 };
